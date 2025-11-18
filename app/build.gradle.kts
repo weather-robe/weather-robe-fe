1255260e2485790b2d224f9a1e53f9f5930a4586
Sejeong Kim <sejeonggim68@gmail.com>
2025-11-19 03:00:25 +0900

    feat: 하단 넵바 생성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
