28d0e9988044468973e36a2f03e044c904c3d61a
Sejeong Kim <sejeonggim68@gmail.com>
2025-11-20 20:47:35 +0900

    feat: 컴포넌트 분리

--- app/src/main/java/com/cookandroid/weatherrobe/BottomNavigation.java
+++ app/src/main/java/com/cookandroid/weatherrobe/BottomNavigation.java
@@ -6,41 +6,56 @@ import android.widget.LinearLayout;
 
 public class BottomNavigation {
 
+    public interface OnTabSelectedListener {
+        void onTabSelected(int tabId);
+    }
+
     private static int dp(int value) {
         return (int) (value * Resources.getSystem().getDisplayMetrics().density);
     }
 
-    public static void resetTabs(
-            LinearLayout innerHome,
-            LinearLayout innerHourly,
-            LinearLayout innerDaily,
-            LinearLayout innerCalendar
+    public static void setup(
+            View bottomNav,
+            OnTabSelectedListener listener
     ) {
-        innerHome.setBackgroundResource(0);
-        innerHourly.setBackgroundResource(0);
-        innerDaily.setBackgroundResource(0);
-        innerCalendar.setBackgroundResource(0);
-    }
+        LinearLayout tabHome = bottomNav.findViewById(R.id.tab_home);
+        LinearLayout tabHourly = bottomNav.findViewById(R.id.tab_hourly);
+        LinearLayout tabDaily = bottomNav.findViewById(R.id.tab_daily);
+        LinearLayout tabCalendar = bottomNav.findViewById(R.id.tab_calendar);
 
-    public static void selectTab(
-            View selected,
-            LinearLayout innerHome,
-            LinearLayout innerHourly,
-            LinearLayout innerDaily,
-            LinearLayout innerCalendar
-    ) {
-        resetTabs(innerHome, innerHourly, innerDaily, innerCalendar);
+        LinearLayout innerHome = bottomNav.findViewById(R.id.inner_home);
+        LinearLayout innerHourly = bottomNav.findViewById(R.id.inner_hourly);
+        LinearLayout innerDaily = bottomNav.findViewById(R.id.inner_daily);
+        LinearLayout innerCalendar = bottomNav.findViewById(R.id.inner_calendar);
 
-        int id = selected.getId();
+        Runnable reset = () -> {
+            innerHome.setBackgroundResource(0);
+            innerHourly.setBackgroundResource(0);
+            innerDaily.setBackgroundResource(0);
+            innerCalendar.setBackgroundResource(0);
+        };
 
-        if (id == R.id.tab_home) {
-            innerHome.setBackgroundResource(R.drawable.bg_selected_tab);
-        } else if (id == R.id.tab_hourly) {
-            innerHourly.setBackgroundResource(R.drawable.bg_selected_tab);
-        } else if (id == R.id.tab_daily) {
-            innerDaily.setBackgroundResource(R.drawable.bg_selected_tab);
-        } else if (id == R.id.tab_calendar) {
-            innerCalendar.setBackgroundResource(R.drawable.bg_selected_tab);
-        }
+        View.OnClickListener click = v -> {
+            reset.run(); // 전체 초기화
+
+            if (v.getId() == R.id.tab_home) {
+                innerHome.setBackgroundResource(R.drawable.bg_selected_tab);
+            } else if (v.getId() == R.id.tab_hourly) {
+                innerHourly.setBackgroundResource(R.drawable.bg_selected_tab);
+            } else if (v.getId() == R.id.tab_daily) {
+                innerDaily.setBackgroundResource(R.drawable.bg_selected_tab);
+            } else if (v.getId() == R.id.tab_calendar) {
+                innerCalendar.setBackgroundResource(R.drawable.bg_selected_tab);
+            }
+
+            listener.onTabSelected(v.getId());
+        };
+
+        tabHome.setOnClickListener(click);
+        tabHourly.setOnClickListener(click);
+        tabDaily.setOnClickListener(click);
+        tabCalendar.setOnClickListener(click);
+
+        click.onClick(tabHome);
     }
 }
--- app/src/main/java/com/cookandroid/weatherrobe/MainActivity.java
+++ app/src/main/java/com/cookandroid/weatherrobe/MainActivity.java
@@ -5,12 +5,8 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
-    private LinearLayout tabHome, tabHourly, tabDaily, tabCalendar;
-    private LinearLayout innerHome, innerHourly, innerDaily, innerCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,37 +15,16 @@ public class MainActivity extends AppCompatActivity {
 
         View bottomNav = findViewById(R.id.bottom_nav);
 
-        tabHome = bottomNav.findViewById(R.id.tab_home);
-        tabHourly = bottomNav.findViewById(R.id.tab_hourly);
-        tabDaily = bottomNav.findViewById(R.id.tab_daily);
-        tabCalendar = bottomNav.findViewById(R.id.tab_calendar);
-
-        innerHome = bottomNav.findViewById(R.id.inner_home);
-        innerHourly = bottomNav.findViewById(R.id.inner_hourly);
-        innerDaily = bottomNav.findViewById(R.id.inner_daily);
-        innerCalendar = bottomNav.findViewById(R.id.inner_calendar);
-
-        replaceFragment(new HomeFragment());
-        BottomNavigation.selectTab(tabHome, innerHome, innerHourly, innerDaily, innerCalendar);
-
-        tabHome.setOnClickListener(v -> {
-            BottomNavigation.selectTab(v, innerHome, innerHourly, innerDaily, innerCalendar);
-            replaceFragment(new HomeFragment());
-        });
-
-        tabHourly.setOnClickListener(v -> {
-            BottomNavigation.selectTab(v, innerHome, innerHourly, innerDaily, innerCalendar);
-            replaceFragment(new HourlyFragment());
-        });
-
-        tabDaily.setOnClickListener(v -> {
-            BottomNavigation.selectTab(v, innerHome, innerHourly, innerDaily, innerCalendar);
-            replaceFragment(new DailyFragment());
-        });
-
-        tabCalendar.setOnClickListener(v -> {
-            BottomNavigation.selectTab(v, innerHome, innerHourly, innerDaily, innerCalendar);
-            replaceFragment(new CalendarFragment());
+        BottomNavigation.setup(bottomNav, tabId -> {
+            if (tabId == R.id.tab_home) {
+                replaceFragment(new HomeFragment());
+            } else if (tabId == R.id.tab_hourly) {
+                replaceFragment(new HourlyFragment());
+            } else if (tabId == R.id.tab_daily) {
+                replaceFragment(new DailyFragment());
+            } else if (tabId == R.id.tab_calendar) {
+                replaceFragment(new CalendarFragment());
+            }
         });
     }
 
