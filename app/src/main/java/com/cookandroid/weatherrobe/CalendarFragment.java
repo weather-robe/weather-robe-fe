d6d88e0c3f69795a279a28beec71af83eaa7c4fc
SARA-MAYO <seolha.lee423@gmail.com>
2025-11-22 04:38:00 +0900

    fix: 기존 CalendarActivity파일 삭제 후, CalenderFragment파일 추가

--- app/src/main/java/com/cookandroid/weatherrobe/CalendarFragment.java
+++ app/src/main/java/com/cookandroid/weatherrobe/CalendarFragment.java
@@ -4,19 +4,43 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class CalendarFragment extends Fragment {
 
     public CalendarFragment() {
+        // 기본 생성자
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater,
-                             ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.fragment_calendar, container, false);
     }
-}
+
+    @Override
+    public void onViewCreated(@NonNull View view,
+                              @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // 캘린더 헤더 설정
+        setupHeaderForCalendar(view);
+    }
+
+    private void setupHeaderForCalendar(View view) {
+        ImageView mapIcon = view.findViewById(R.id.header_map_icon);
+        TextView title = view.findViewById(R.id.header_title);
+
+        mapIcon.setVisibility(View.GONE);
+        title.setText("2025년 10월");
+    }
+
+// 하단 네비바 추가 필요함!
