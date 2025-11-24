609c24a1adce85397b1f07a1cdf40581b0a03f7a
SARA-MAYO <seolha.lee423@gmail.com>
2025-11-24 19:57:26 +0900

    외않되

--- app/src/main/java/com/cookandroid/weatherrobe/ApiClient.java
+++ app/src/main/java/com/cookandroid/weatherrobe/ApiClient.java
@@ -3,17 +3,18 @@ package com.cookandroid.weatherrobe;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class ApiClient{
-    private static Retro retrofit = null;
+public class ApiClient {
 
-    public static Retrofit getClient(){
-        if(retrofit == null){
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl("http://192.168.0.2:8080")
+                    .baseUrl("http://192.168.0.2:8080/")  // 일단 캘린더 API는 내 로컬 서버(설하)로 돌리는 중
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
         return retrofit;
-        }
     }
 }
+// ..
--- app/src/main/java/com/cookandroid/weatherrobe/CalendarFragment.java
+++ app/src/main/java/com/cookandroid/weatherrobe/CalendarFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class CalendarFragment extends Fragment {
 
@@ -30,9 +33,6 @@ public class CalendarFragment extends Fragment {
     public void onViewCreated(@NonNull View view,
                               @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        // 캘린더 전용 헤더 설정
-        setupHeaderForCalendar(view);
 
         // 서버 날짜 불러오기
         loadMonthFromServer(view);
@@ -56,17 +56,8 @@ public class CalendarFragment extends Fragment {
 
             @Override
             public void onFailure(Call<MonthResponse> call, Throwable t) {
-                // 실패 처리
+                // 서버 요청 실패 처리: 현재 연결 실패 시 아무 변화 없음
             }
         });
     }
-
-    private void setupHeaderForCalendar(View view) {
-        ImageView leftIcon = view.findViewById(R.id.ivLeft);     // 왼쪽 햄버거 btn
-        TextView monthTitle = view.findViewById(R.id.tvMonth);   // 현재 현재 날짜 영역
-        ImageView rightIcon = view.findViewById(R.id.ivInfo);     // 오른쪽 더 보기 btn
-
-        monthTitle.setText("2025년 10월");  // 현재 날짜 영역: 추후 API 연동 예정임. 임시데이터
-    }
 }
-// 하단 네비바 추가 필요함!
