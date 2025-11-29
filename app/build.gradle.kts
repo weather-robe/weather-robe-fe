ca681234fefebab595214cab413797b7f611f08f
SARA-MAYO <seolha.lee423@gmail.com>
2025-11-29 23:16:32 +0900

    chore: 사용자 실제 체감온도 기능을 위한 WorkManager 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.work:work-runtime:2.9.0")
+
 }
