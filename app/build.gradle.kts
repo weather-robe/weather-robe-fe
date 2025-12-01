25a85c53f5d09c6902f242eadc72592769325955
strfunctionk <comzun01@gmail.com>
2025-12-01 20:04:05 +0900

    feat: 의존성 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:5.3.2")
     implementation("com.facebook.shimmer:shimmer:0.5.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-
+    implementation ("com.github.bumptech.glide:glide:5.0.5")
+    annotationProcessor("com.github.bumptech.glide:compiler:5.0.5")
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
 }
