557d2a145c3f0defed902f55e69dd276bfef8e03
Sejeong Kim <sejeonggim68@gmail.com>
2025-12-03 12:25:48 +0900

    feat: 모달 전역 관리

--- app/src/main/java/com/cookandroid/weatherrobe/Home/model/SharedWeatherViewModel.java
+++ app/src/main/java/com/cookandroid/weatherrobe/Home/model/SharedWeatherViewModel.java
@@ -4,9 +4,12 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.List;
+
 public class SharedWeatherViewModel extends ViewModel {
     private final MutableLiveData<Integer> selectedWeatherId = new MutableLiveData<>();
     private final MutableLiveData<String> selectedWeatherFeedback = new MutableLiveData<>();
+    private final MutableLiveData<List<String>> selectedKeys = new MutableLiveData<>();
 
     public LiveData<Integer> getWeatherId() {
         return selectedWeatherId;
@@ -22,5 +25,13 @@ public class SharedWeatherViewModel extends ViewModel {
 
     public void setWeatherFeedback(String feedback) {
         selectedWeatherFeedback.setValue(feedback);
+    }
+
+    public LiveData<List<String>> getSelectedKeys() {
+        return selectedKeys;
+    }
+
+    public void setSelectedKeys(List<String> keys) {
+        selectedKeys.setValue(keys);
     }
 }
